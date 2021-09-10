63e95fe1001bcbc3486b8ea98ac84840203e493d
oddlyspaced <oddlyspaced1@gmail.com>
2021-09-11 03:00:36 +0530

    ApiClient: Simplify get client method

--- app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
+++ app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
@@ -7,24 +7,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
-    private lateinit var client: Api
-
     fun getApiClient(): Api {
-        if (!this::client.isInitialized) {
-            val gson = GsonBuilder()
-                .setLenient()
-                .create()
-            val okHttpClient = OkHttpClient.Builder()
-                .readTimeout(5000, TimeUnit.SECONDS)
-                .connectTimeout(100, TimeUnit.SECONDS)
-                .build()
-            client = Retrofit.Builder()
-                .baseUrl(Global.API_URL)
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
-                .create(Api::class.java)
-        }
-        return client
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val okHttpClient = OkHttpClient.Builder()
+            .readTimeout(5000, TimeUnit.SECONDS)
+            .connectTimeout(100, TimeUnit.SECONDS)
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(Global.API_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(Api::class.java)
     }
 }
