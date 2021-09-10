63d7125ca73c6208f043cb7c6a133fcff90679ec
oddlyspaced <oddlyspaced1@gmail.com>
2021-09-11 02:59:53 +0530

    ApiClient: Copy over

--- app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
+++ app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
@@ -1,4 +1,30 @@
 package com.oddlyspaced.omdb
 
-class ApiClient {
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+    private lateinit var client: Api
+
+    fun getApiClient(): Api {
+        if (!this::client.isInitialized) {
+            val gson = GsonBuilder()
+                .setLenient()
+                .create()
+            val okHttpClient = OkHttpClient.Builder()
+                .readTimeout(5000, TimeUnit.SECONDS)
+                .connectTimeout(100, TimeUnit.SECONDS)
+                .build()
+            client = Retrofit.Builder()
+                .baseUrl(Global.API_URL)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(Api::class.java)
+        }
+        return client
+    }
 }
