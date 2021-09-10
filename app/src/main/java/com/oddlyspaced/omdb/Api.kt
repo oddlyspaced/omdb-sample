d6fc7148ea3c181fc0f174d700b1e5c51dec9a8f
oddlyspaced <oddlyspaced1@gmail.com>
2021-09-11 03:15:24 +0530

    Api: Add search end point

--- app/src/main/java/com/oddlyspaced/omdb/Api.kt
+++ app/src/main/java/com/oddlyspaced/omdb/Api.kt
@@ -1,4 +1,16 @@
 package com.oddlyspaced.omdb
 
-class Api {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+
+    @GET
+    fun searchMovies(
+        @Query("apikey") apiKey: String = Global.API_KEY,
+        @Query("s") searchQuery: String,
+        @Query("page") pageNumber: Int,
+    ): Call<SearchResultWrapper>
+
 }
