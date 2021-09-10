6ae3f641ef6b88aa23f9625ae52f99303174a8e8
oddlyspaced <oddlyspaced1@gmail.com>
2021-09-11 03:52:52 +0530

    ApiClient: Add loggin interceptor

--- app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
+++ app/src/main/java/com/oddlyspaced/omdb/ApiClient.kt
@@ -2,16 +2,22 @@ package com.oddlyspaced.omdb
 
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object ApiClient {
     fun getApiClient(): Api {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
         val gson = GsonBuilder()
             .setLenient()
             .create()
         val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
             .readTimeout(5000, TimeUnit.SECONDS)
             .connectTimeout(100, TimeUnit.SECONDS)
             .build()
